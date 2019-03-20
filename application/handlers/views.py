945e18e0552902e62bccd273ac4f6100c70c8915
Hanna Jantunen <hanna.r.jantunen@helsinki.fi>
2019-03-20 07:55:48 +0200

    muokkaustoiminto lisätty

--- application/handlers/views.py
+++ application/handlers/views.py
@@ -4,11 +4,24 @@ from application.handlers.models import Handler
 
 @app.route("/handlers", methods=["GET"])
 def handlers_index():
-    return render_template("handlers/list.html", handlers = Handler.query.all() )
+    return render_template("handlers/list.html", handlers= Handler.query.all() )
 
 @app.route("/handlers/new/")
 def handlers_form():
     return render_template("handlers/new.html")
+
+@app.route("/handlers/modify_<handler_id>/", methods=["GET"])
+def handlers_modify_form(handler_id):
+    return render_template("handlers/modify.html", handler_id=handler_id)
+
+@app.route("/handlers/<handler_id>/", methods=["POST"])
+def handlers_set_name(handler_id):
+    h = Handler.query.get(handler_id)
+    h.name = request.form.get("newname")
+    db.session().commit()
+
+    return redirect(url_for("handlers_index"))
+
 
 @app.route("/handlers/", methods=["POST"])
 def handlers_create():
