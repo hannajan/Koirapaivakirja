c5df9e4eaa27f49357a8843d436f8e0d2d17e54e
Hanna Jantunen <hanna.r.jantunen@helsinki.fi>
2019-05-03 18:57:19 +0300

    Toinen yhteenvetokysely lisätty
    Lisätty toinen yhteenvetokysely, jonka toimivuutta ei vielä testattu.

--- application/dogs/models.py
+++ application/dogs/models.py
@@ -1,6 +1,7 @@
 import datetime
 
 from application import db
+from sqlalchemy import text
 
 class Dog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,3 +15,18 @@ def __init__(self, name, breed, birthday):
     self.name = name
     self.breed = breed
     self.birthday = birthday
+
+
+    @staticmethod
+    def count_walked_km_for_dog(dog_id):
+        stmt = text("SELECT Dog.name, COUNT(Walk.length) FROM Walk"
+                    " JOIN DogWalk ON DogWalk.walk_id = Walk.id"
+                    " JOIN Dog ON Dog.id = DogWalk.dog_id"
+                    " WHERE Dog.id = :dog_id;").params(dog_id=dog_id)
+
+        res = db.engine.execute(stmt)
+        response = []
+        for row in res:
+            response.append({"name":row[0], "km":row[1]})
+
+        return response
